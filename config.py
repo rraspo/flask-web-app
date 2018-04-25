4beb0f6930a4961c2d33af9a1fffb2be604cb530
Juan A. Porras <juanabrahamporras@gmail.com>
2018-04-25 17:32:14 -0500

    Updated configuration for different environments

--- config.py
+++ config.py
@@ -1,4 +1,28 @@
 # config.py
 
-# Flask debugging
-DEBUG = True
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+class DevelopmentConfig(Config):
+    """
+    Development configuration
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configuration
+    """
+
+    DEBUG = True
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
