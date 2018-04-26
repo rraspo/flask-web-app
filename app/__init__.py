93c250839535e94f88199068f7292fd0d4dfc506
Juan A. Porras <juanabrahamporras@gmail.com>
2018-04-26 02:48:54 -0500

    Updated file for a higher semantyc value

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@
 
 # external imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
@@ -20,13 +20,13 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
 
     # variables binding
+    Bootstrap(app)
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
     migrate = Migrate(app, db)
     
-    Bootstrap(app)
     
     from app import models
 
